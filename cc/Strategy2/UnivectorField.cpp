8e32ef0f82282b5145aefe9daa22ea0ad8143130
pqvs <pqverysmall@gmail.com>
2019-08-29 10:53:44 -0300

    Continuar implementacao

---
+++ cc/Strategy2/UnivectorField.cpp
@@ -0,0 +1,32 @@
+#include "UnivectorField.h"
+
+using namespace Geometry;
+using namespace field;
+using namespace Robot;
+
+void UnivectorField::updateConstants(const UnivectorField::UVF_params params) {
+	UVF_params = params;
+	//avoidField.updateParam(self.k0)
+	//moveField.updateParams(self.kr, self.radius)
+}
+
+void UnivectorField::updateRobot(const Geometry::Point &ball) {
+	Vector ball_to_goal = their::goal::back::center - ball;
+}
+
+void UnivectorField::univector(const Geometry::Point &ball) {
+//	this->updateRobot(get_pose());
+//	if(orientation == null){
+//		orientation = {650.0, 250.0};
+//	}
+
+	fi_auf = 0.0;
+	minDistance = UVF_params::dMin + 1;
+	Pose obstacles[];
+	Position centers[] = new Position[5];
+	//obstacles = nullptr; para desativar os obstaculos
+
+	for (int i = 0; i == obstacles.length; i++) {
+		centers[i] = obstacles[i].position;
+	}
+}
---
+++ cc/Strategy2/UnivectorField.h
@@ -0,0 +1,40 @@
+#ifndef VSSS_UNIVECTOR_FIELD_H
+#define VSSS_UNIVECTOR_FIELD_H
+
+#include "boost/variant.hpp"
+#include "Robot2.h"
+#include "Geometry/Geometry.h"
+#include "Field.h"
+
+class UnivectorField : public Robot2 {
+	public:
+
+		struct UVF_params {	// Parametros utilizados no UVF
+			double radius;
+			double kr;
+			double k0;
+			double dMin;
+			double lDelta;
+		};
+
+		void updateConstants(const UnivectorField::UVF_params params);
+
+
+		void updateRobot(const Geometry::Point &ball);
+
+
+		void updateTarget(const Geometry::Point &ball);
+
+
+		void updateObstacles(Geometry:: Point ball);
+
+
+		void univector(const Geometry::Point& ball);
+
+	//private:
+	//	Role get_role() override { return Role::Attacker; };
+
+	//	void decide_spin_shot(const Geometry::Point &ball);
+};
+
+#endif //VSSS_ATTACKER_H
